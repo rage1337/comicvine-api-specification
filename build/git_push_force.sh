8ed47011af6ff4b1807fdc702ad21013b50d5ff2
guillaumedsde <guillaume@desusanne.com>
2020-06-01 12:52:36 +0200

    ci: change git push script

--- build/git_push_force.sh
+++ build/git_push_force.sh
@@ -29,20 +29,7 @@ if [ "$release_note" = "" ]; then
     echo "[INFO] No command line input provided. Set \$release_note to $release_note"
 fi
 
-# Initialize the local directory as a Git repository
-git init
-
-git stash
-
-git pull origin master
-
-git stash apply
-
-# Adds the files in the local repository and stages them for commit.
-git add .
-
-# Commits the tracked changes and prepares them to be pushed to a remote repository.
-git commit -m "$release_note"
+repo_loc=../../target/
 
 # Sets the new remote
 git_remote=$(git remote)
@@ -50,12 +37,22 @@ if [ "$git_remote" = "" ]; then # git remote not defined
 
     if [ "$GIT_TOKEN" = "" ]; then
         echo "[INFO] \$GIT_TOKEN (environment variable) is not set. Using the git credential in your environment."
-        git remote add origin https://${git_host}/${git_user_id}/${git_repo_id}.git
+        git clone https://${git_host}/${git_user_id}/${git_repo_id}.git ${repo_loc}
     else
-        git remote add origin https://${git_user_id}:${GIT_TOKEN}@${git_host}/${git_user_id}/${git_repo_id}.git
+        git clone https://${git_user_id}:${GIT_TOKEN}@${git_host}/${git_user_id}/${git_repo_id}.git ${repo_loc}
     fi
 
 fi
+
+mv ./* ${repo_loc}
+
+cd ${repo_loc}
+
+# Adds the files in the local repository and stages them for commit.
+git add .
+
+# Commits the tracked changes and prepares them to be pushed to a remote repository.
+git commit -m "$release_note"
 
 # Pushes (Forces) the changes in the local repository up to the remote repository
 echo "Git pushing to https://${git_host}/${git_user_id}/${git_repo_id}.git"
